7808ac2c99fe09ad164bc33bc67e8ba4068d983c
Welnyr <thekotech@gmail.com>
2022-10-19 13:05:20 +0500

    InputPanel.jsx refactor

--- src/components/InputPanel.jsx
+++ src/components/InputPanel.jsx
@@ -10,40 +10,32 @@ export default class InputPanel extends Component {
 	}
 
 	render() {
+
+		// В каждом поле можно указать по доп. объекту, в каджом свой defaultValue.
+		const inputLabels = {
+			numOfCashiers: 'Количество касс',
+			customersInterval: 'Интервал',
+			customersPerInterval: 'Количество',
+			minServeTime: 'Мин. время обслуживания',
+			maxServeTime: 'Макс. время обслуживания'
+		}
+
+		const inputFields = []
+		for (const property in inputLabels) {
+			inputFields.push(
+				<InputField
+					id={property}
+					label={inputLabels[property]}
+					onChange={e => this.props.onChange(e)}
+					defaultValue={1}
+					key={inputLabels[property]}
+				/>
+			)
+		}
+
 		return (
-			// todo: убрать повторение кода.
-			// Возможно, использовать массив объектов?
-			<div className='attribute_panel'>
-				<InputField
-					id='numOfCashiers'
-					label='Количество касс'
-					onChange={(e) => this.props.onChange(e)}
-					defaultValue={1}
-				/>
-				<InputField
-					id='customersInterval'
-					label='Интервал'
-					onChange={(e) => this.props.onChange(e)}
-					defaultValue={1}
-				/>
-				<InputField
-					id='customersPerInterval'
-					label='Количество'
-					onChange={(e) => this.props.onChange(e)}
-					defaultValue={1}
-				/>
-				<InputField
-					id='minServeTime'
-					label='Мин. время обслуживания'
-					onChange={(e) => this.props.onChange(e)}
-					defaultValue={1}
-				/>
-				<InputField
-					id='maxServeTime'
-					label='Макс. время обслуживания'
-					onChange={(e) => this.props.onChange(e)}
-					defaultValue={1}
-				/>
+			<div className='input_panel'>
+				{inputFields}
 			</div>
 		)
 	}
