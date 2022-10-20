757ea11db297bde84cf0c1b6b91c93e7697cdf2f
TheKoTech <thekotech@gmail.com>
2022-10-20 23:14:59 +0500

    styling

--- src/components/Cashier.jsx
+++ src/components/Cashier.jsx
@@ -3,20 +3,24 @@ import PropTypes, { number } from 'prop-types';
 
 
 export default class Cashier extends Component {
-	
-	 static propTypes = {
+
+	static propTypes = {
 		numOfCustomers: PropTypes.number,
 		minTime: PropTypes.number,
 		maxTime: PropTypes.number
 	}
-	
-	constructor(props) {
-		super(props)
-	}
 
 	render() {
 		return (
-			<div>Cashier</div>
+
+			<div className='cashier'>
+				<h3>{this.props.title}</h3>
+				<div className='cashier_inputs'>
+					<span className='queue_icon' />
+					{/* todo: finish the card. Add mintime and maxtime. They must be overriden by state */}
+					<span className='queue'>{this.props.numOfCustomers}</span>
+				</div>
+			</div>
 		)
 	}
 }
