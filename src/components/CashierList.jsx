179ad021ff6bf3293ba507d4e75507d3703e0044
TheKoTech <thekotech@gmail.com>
2022-10-29 22:44:43 +0500

    Reverted back to where I was stuck, but with a function component

--- src/components/CashierList.jsx
+++ src/components/CashierList.jsx
@@ -2,33 +2,55 @@ import React, { useEffect, useState } from 'react'
 import Cashier from './Cashier'
 
 
-function randInt(max) {
-	return Math.floor(Math.random() * max)
-}
-
 function CashierList(props) {
+
+	function randInt(max) {
+		return Math.floor(Math.random() * max)
+	}
+
+	const [cashierList, setCashierList] = useState()
+
+	useEffect(() => {
+
+		const cashierKeys = []
+		const cashierQueueLengths = []
+
+		for (let i = 0; i < props.numOfCashiers; i++) {
+			cashierKeys.push(i)
+			cashierQueueLengths.push(0)
+		}
+
+		setCashierList(
+			cashierKeys.map((key, i) =>
+				<Cashier
+					key={key}
+					title={'Cashier ' + (key + 1)}
+					numOfCustomers={cashierQueueLengths[i]}
+					minTime={props.minServeTime}
+					maxTime={props.maxServeTime}
+				/>
+			)
+		)
+	}, [props.numOfCashiers])
 
 	let timeTillNextCustomers = randInt(props.customersInterval)
 
 	useEffect(() => {
-		const timer = setInterval(() => tick(), 1000)
+		const timer = setInterval(() => {
+			if (timeTillNextCustomers === 0) {
+				timeTillNextCustomers = randInt(props.customersInterval)
+			} else {
+				timeTillNextCustomers--
+			}
+			console.log(timeTillNextCustomers)
+		}, 1000)
 		return () => clearInterval(timer)
 	})
 
 
-	function tick() {
-		if (timeTillNextCustomers === 0) {
-			timeTillNextCustomers = randInt(props.customersInterval)
-		} else {
-			timeTillNextCustomers--
-		}
-		console.log(timeTillNextCustomers)
-	}
-
-
 	return (
 		<div className='cashier_list'>
-
+			{cashierList}
 		</div>
 	)
 
